8a3270dac893b040933a56af8888f2a087dd76b2
jayant <jayantyadav366@gmail.com>
2025-12-17 00:07:19 +0530

    aaluu ke chalu

---
+++ src/main/java/com/example/demo/TestGit3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGit3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGit3Application.class, args);
+	}
+
+}
